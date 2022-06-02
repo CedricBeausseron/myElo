a8ab39b29d2ab5ebc1d043647ca6c5eeda4b82f4
C <38694948+CedricBeausseron@users.noreply.github.com>
2022-06-02 14:01:44 +0200

    commit

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,7 +2,7 @@ let rootPath
 if(! window.location.href.includes("https://cedricbeausseron.github.io/myElo")) {
     rootPath = "nophp/myElo/"
 }else{
-    rootPath = "/myElo/"
+    rootPath = "https://cedricbeausseron.github.io/myElo/"
 }
 
 //appeler une fonction par son nom en string
